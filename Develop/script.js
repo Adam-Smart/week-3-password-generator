e5c035c7e5e1557fbd7d3f505205a78c57599d0b
Adam-Smart <adams8632@hotmail.co.uk>
2023-04-17 21:15:45 +0100

    added characters for password gen

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = genPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+function genPassword(){
+
+  const uppercase = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+  const lowercase = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+  const number = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+  const specialcharacter = "!, £, $, %, ^, &, *, (, ), _, -, =, +, {, }, [, ], @, #, ~, ?, <, >"
+
+  return("ifik32dieh1fe")
 }
 
 // Add event listener to generate button
