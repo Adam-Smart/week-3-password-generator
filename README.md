95ef31b63046ee31dc208f4e2e585009a96f63a3
Adam-Smart <adams8632@hotmail.co.uk>
2023-04-21 07:13:49 +0100

    added text, image and deploy link  to readme.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # week-3-password-generator
+
+## Description
+This is a webpage which will generate a random and secure password using a series of promts. The series of prompts  will go through a few things which will be the amount of charcters the user would like and which characters they would like included such as "Uppercase", "Lowercase", "Numbers" and "Special" characters.
+
+The prompts have been set up to have a few requirements and if the requiremnts are not fulfilled the application will cancel out and will require the user to try again. The two requirments that i have set are that the user has a password length set between 8 and 128 characters and that the user selects at least one character type set.
+
+Once the user has completed all the promts and they have fulfilled the requirements the password is the generated with all of the random characters they have selected to the set legnth they have chosen.
+
+## Problems and solutions 
+
+One of the main issuses i ran into was when i was generating the password was it was including spaces and some of the commas which was not ideal. To fix the issue with the extra spaces i just had to simply go through the arrays and remove them as i had added them by mistake. To fix the commas problem i had to go into the "var password" which at the time was wrong and looked like the example below.
+
+if(passwordUpper) {
+avaliblecharacters.push(uppercase.concat(""));
+}
+
+To fix this I had to change the "concat" to "split" and add the comma into the quote marks. The split fixed this issue as it was adding everything into the array but spliting the commas out of it. 
+
+## Image
+![Image of the Password Generator](/Assets/PasswordGenDone.jpg)
+
+## Link to Password Generator
+https://adam-smart.github.io/week-3-password-generator/ 
+## License
+N/A
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // this is a function which will dipslay the random password into the card once its generated
+
 function writePassword() {
   var password = genPassword();
   var passwordText = document.querySelector("#password");
@@ -10,63 +11,87 @@ function writePassword() {
 
 }
 //This is a fuction which will generate a random passowrd
+
 function genPassword(){
+
 // This is multiple of arrays of characters which can be generated
+
   const uppercase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
   const lowercase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
   const number = "0,1,2,3,4,5,6,7,8,9";
-  const specialcharacter = "!,£,$,%,^,&,*,(,),_,-,=,+,{,},[,],@,#,~,?,<,>"
+  const specialcharacter = "!,£,$,%,^,&,*,(,),_,-,=,+,{,},[,],@,#,~,?,<,>";
 
   var passwordLength = 0;
 var passwordUpper = false;
 var passwordLower = false;
 var passwordNum = false;
 var passwordSpec = false;
+
 // This displays a promt at the top of the page displaying the message below with a box allowing the user to enter the desired length of the password with a confim and cancel button
+
 passwordLength = window.prompt("Enter how many characters are wanted in the password. Must be between 8 and 128! ");
+
 // This displays a alert alerting the user that the password must contain between 8 and 128 characters 
+
 if (passwordLength < 8 ||  passwordLength > 128) {
   window.alert ("Must be between 8 and 128 characters! ");
 return;
+
 }
 // This is a set of confim promt asking what type of characters the user would like using "OK" or "cancel" button in the promt 
+
 passwordUpper = window.confirm("Confirm for Uppercase");
 passwordLower = window.confirm("Confirm for Lowercase");
 passwordNum = window.confirm("Confirm for Numbers");
 passwordSpec = window.confirm("Confirm for Secial characters")
+
 // This if statement checks to see if the user has at least acepted at least on of the character types and if not will display the message below and close out
+
 if (passwordUpper === false && passwordLower === false && passwordNum === false && passwordSpec === false){
   window.alert("Must enter at least one character type.")
 }
+
 // This is a varible which will go through and see what options the user have selected and then add the selected array and push them into one array called "avaliblecharactters" 
+
 var password = "" ;
 const avaliblecharacters = [];
+
 // Checks for if the user has used uppercase characters
+
 if(passwordUpper) {
 avaliblecharacters.push(uppercase.split(","));
 }
+
 // Checks for if the user has used lower characters
+
 if (passwordLower){
 avaliblecharacters.push(lowercase.split(","));
 }
+
 // Checks for if the user has used number characters
+
 if (passwordNum){
   avaliblecharacters.push(number.split(","));
+
 }
 // Checks for if the user has used special characters
+
 if (passwordSpec){
   avaliblecharacters.push(specialcharacter.split(","));
+
 }
+
 // This for loop will go through the "avaliblecharacters" array and select a random character. This will loop until it gets to the end of the passwordlegnth 
+
 for (var i=0; i <passwordLength; i++){
   var randomChar = avaliblecharacters[Math.floor(Math.random() * avaliblecharacters.length)];
   password += randomChar[Math.floor(Math.random() * randomChar.length)];
 }
+
 // This then retuns the generated password 
+
 return(password)
-
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
